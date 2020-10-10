d38d8073078c19e2243ceba79d2f0a3820994afb
Lenon <lenon.paula@bling.com.br>
2020-10-10 17:01:19 -0300

    Remove importações desnecessárias

--- App.js
+++ App.js
@@ -8,24 +8,11 @@
 
 import React, {useState} from 'react';
 import {
-	SafeAreaView,
 	StyleSheet,
-	ScrollView,
 	View,
 	Text,
-	StatusBar,
 	Button
 } from 'react-native';
-
-
-import {
-	Header,
-	LearnMoreLinks,
-	Colors,
-	DebugInstructions,
-	ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 
 function Todo({ todo, index, completeTodo, removeTodo }) {
 	return (
