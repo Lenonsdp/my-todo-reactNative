9148be6bd67e7ea795175976ff8eea63e65d9cbf
Lenon <lenon.paula@bling.com.br>
2020-10-10 16:58:53 -0300

    Upload project

---
+++ App.js
@@ -0,0 +1,126 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {
+	SafeAreaView,
+	StyleSheet,
+	ScrollView,
+	View,
+	Text,
+	StatusBar,
+	Button
+} from 'react-native';
+
+
+import {
+	Header,
+	LearnMoreLinks,
+	Colors,
+	DebugInstructions,
+	ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+	return (
+		<View style={styles.todo}>
+			<Text style={{ textDecorationLine: todo.isCompleted ?
+				"line-through" : "none" }}>
+				{todo.text}
+			</Text>
+			<Button style={styles.button}
+				onPress={() => completeTodo(index)}
+				title='Concluir'				
+			/>
+			<Button style={styles.button}
+				onPress={() => removeTodo(index)}
+				title='Remover'
+			/>
+		</View>
+	);
+}
+
+const App: () => React$Node = () => {
+	const [todos, setTodos] = useState([
+			{ text: "Aprender sobre o React",
+			isCompleted: false },
+			{ text: "Encontrar um amigo para o almoço",
+			isCompleted: false },
+			{ text: "Passar no supermercado",
+			isCompleted: false }
+		]);
+		const addTodo = info => {
+			const newTodos = [...todos, { text: info, isCompleted:false }];
+			setTodos(newTodos);
+		};
+		
+		const completeTodo = index => {
+			const newTodos = [...todos];
+			newTodos[index].isCompleted = true;
+			setTodos(newTodos);
+		};
+	
+		const removeTodo = index => {
+			const newTodos = [...todos];
+			newTodos.splice(index, 1);
+			setTodos(newTodos);
+		};
+		return (
+			<View style={styles.container}>
+				<View style={styles.todoList}>
+					{todos.map((todo, index) => (
+						<Todo key={index}
+							index={index}
+							todo={todo}
+							completeTodo={completeTodo}
+							removeTodo={removeTodo}
+						/>					
+					))}
+					<Button style={styles.button}
+						onPress={() => addTodo("Mais um item")}
+						title='Adicione um item'
+					/>
+				</View>
+			</View>
+		);
+};
+
+const styles = StyleSheet.create({
+
+	container: {
+		flex: 1,
+		backgroundColor: "#20FFee",
+		padding: 30,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	todoList: {
+		backgroundColor: '#e8e8e8',
+		borderRadius: 4,
+		padding: 5,
+		maxWidth: 400
+	},
+	todo: {
+		backgroundColor: '#fff',
+		padding: 3,
+		fontSize: 12,
+		marginBottom: 6,
+		borderRadius: 3,
+		alignItems: 'center'
+	},
+	button: {
+		padding: 3,
+		fontSize: 12,
+		marginBottom: 6,
+		borderRadius: 3,
+		alignItems: 'center'
+	}
+});
+
+export default App;
